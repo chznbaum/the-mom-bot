b141f4df15422028e832b7322c957fc259f860f8
Chazona Baum <chazona@chazonabaum.com>
2016-10-22 17:06:06 -0400

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	consumer_key: '9Lk02AhjbQN84twXQLKPf71L6',
-	consumer_secret: 'l9i8aGTMOvO3SB6k37fEx31Vxf8kxMae2V13NsEy07bSuc8Qsq',
-	access_token: '788120421778489346-5hsi7teL5UTCYOqM9LUZ3ZdnlCoGF9O',
-	access_token_secret: '3eEGrIAFBDf2D10sTQCh4rBppWwd83w5jzKtVUBKTssvN'
-}
