066c9cd27d21fc2ee8448c9afc1d6184a4d99ebe
Chazona Baum <chznbaum@gmail.com>
2016-10-22 15:02:27 -0400

    separate bot and owner screennames into variables, personalize messages with emoji

--- bot.js
+++ bot.js
@@ -1,38 +1,41 @@
 var Twit = require('twit'); // Include Twit Package
 var config = require('./config'); // Include authentication credentials
-var disappointed_bot_list = [ // What Mom Bot will say when user tweets a bad word
-	"Mom Bot's not mad; she's just disappointed. 😞 Would you like to delete that tweet?",
+var bot_name = 'Mom Bot';
+var bot_screen_name = 'the_mother_bot';
+var bot_owner_name = 'otherconsolelog';
+var disappointed_bot_list = [ // What Bot will say when user tweets a bad word
+	bot_name + "'s not mad; she's just disappointed. 😞 Would you like to delete that tweet?",
 	"You're an adult, but do you really want to keep that tweet? 🤔 You can delete it if you don't want a future boss to see.",
 	"Do you kiss your Mother Bot with that mouth? 😠 You can always delete that tweet if you think it was foolish.",
 	"Sweetie, your tweet had a bad word! 😲 Are you sure that's what you want people to see? You can delete it if not."
 ];
-var thank_you_list = [ // What Mom Bot will say when user follows Mom Bot
-	"Thank you for keeping in touch with Mom Bot, Sweetie. 😘",
+var thank_you_list = [ // What Bot will say when user follows Bot
+	"Thank you for keeping in touch with " + bot_name + ", Sweetie. 😘",
 	"Don't tell your brother, but of course you're my favorite, Sweetie. 😍"
 ];
-var unfollow_bot_list = [ // What Mom Bot will say when Mom Bot unfollows user
-	"Ok Sweetie, Mom Bot will give you space. Follow me again if you want to talk. 😥",
-	"Mom Bot always loves you, but I\'ll leave you alone. Follow me again if you want to talk. 😥",
-	"Ok Sweetie, Mom Bot will miss you, but I'm glad you're having a good time. 😥"
+var unfollow_bot_list = [ // What Bot will say when Bot unfollows user
+	"Ok Sweetie, " + bot_name + " will give you space. Follow me again if you want to talk. 😥",
+	bot_name + " always loves you, but I\'ll leave you alone. Follow me again if you want to talk. 😥",
+	"Ok Sweetie, " + bot_name + " will miss you, but I'm glad you're having a good time. 😥"
 ];
-var feel_better_bot_list = [ // What Mom Bot will say when user tweets about sadness
-	"I'm happy simply because you exist. Just thought you should know that.",
-	"You are the light of my world and the first thing I think about every day.",
-	"Always remember that you are needed and there is work to be done.",
-	"Keep in mind that this, too, will pass. In the meantime, I am here for you in whatever way you need."
+var feel_better_bot_list = [ // What Bot will say when user tweets about sadness
+	"I'm happy simply because you exist. Just thought you should know that. 😘",
+	"You are the light of my world and the first thing I think about every day. 😘",
+	"Always remember that you are needed and there is work to be done. 😉",
+	"Keep in mind that this, too, will pass. In the meantime, I am here for you in whatever way you need. 🤗"
 ];
-var proud_bot_list = [ // What Mom Bot will say when user tweets about something great/to be proud of
-	"I'm so proud of you. And even if you weren't so fantastic, I'd still be proud.",
-	"I believe in you, Sweetie.",
-	"You are one of the best gifts I've ever gotten. I am so proud and humbled.",
-	"I feel so proud when I'm with you.",
-	"You have some real gifts!",
-	"It is so cool to watch you grow up.",
-	"You make me so happy just by being you.",
-	"I love you so much!",
-	"You were born to do this!"
+var proud_bot_list = [ // What Bot will say when user tweets about something great/to be proud of
+	"I'm so proud of you. And even if you weren't so fantastic, I'd still be proud. 😆",
+	"I believe in you, Sweetie. 😘",
+	"You are one of the best gifts I've ever gotten. I am so proud and humbled. 😊",
+	"I feel so proud when I'm with you. 😊",
+	"You have some real gifts! 😆",
+	"It is so cool to watch you grow up. 😆",
+	"You make me so happy just by being you. 😊",
+	"I love you so much!😘",
+	"You were born to do this! 😆"
 ];
-var unfollow_words_list = [ // Words user can include to request Mom Bot to unfollow the user
+var unfollow_words_list = [ // Words user can include to request Bot to unfollow the user
 	"all your fault",
 	"dont care",
 	"dont have to",
@@ -113,21 +116,21 @@ for (k = 0; k < bad_words_list.length; k++) {
 var T = new Twit(config);
 var stream = T.stream('user'); // Setting up a user stream
 stream.on('tweet', tweetEvent); // Anytime a tweet enters the stream, run tweetEvent
-stream.on('follow', followed); // Anytime a user follows Mom Bot, run followed
+stream.on('follow', followed); // Anytime a user follows Bot, run followed
 function tweetEvent(eventMsg) { // Function to run on each tweet in the stream
 	var from = eventMsg.user.screen_name; // Who sent the tweet
 	var text = eventMsg.text; // Message of the tweet
 	var reply_to = eventMsg.in_reply_to_screen_name; // Who tweet was @reply to
-	if (from !== 'the_mother_bot') { // If Mom Bot didn't send the tweet
+	if (from !== bot_screen_name) { // If Bot didn't send the tweet
 		text = text.replace(/[^a-zA-Z\s]/gi, "").toLowerCase(); // Remove non-letter characters and transform to lowercase
 		var tweet_array = text.split(' '); // Create an array of each word in the tweet
 		for (var i = 0; i < tweet_array.length; i++) { // For each word in the tweet
 			if (bad_words_list.indexOf(tweet_array[i]) != -1) { // If the word is included in bad words list
 				var disappointed_text = randomSaying(disappointed_bot_list);
-				tweetIt('@' + from + ' ' + disappointed_text); // Mom Bot tweets her disappointment
+				tweetIt('@' + from + ' ' + disappointed_text); // Bot tweets her disappointment
 			}
 		}
-		if (reply_to !== null && reply_to === 'the_mother_bot') { // If the tweet was @reply to Mom Bot
+		if (reply_to !== null && reply_to === 'bot_screen_name') { // If the tweet was @reply to Bot
 			for (var j = 0; j < unfollow_words_list.length; j++) { // For each word in the unfollow list
 				if (text.indexOf(unfollow_words_list[j]) != -1) { // If an unfollow word is in the tweet
 					var unfollow_text = randomSaying(unfollow_bot_list);
@@ -135,14 +138,14 @@ function tweetEvent(eventMsg) { // Function to run on each tweet in the stream
 					T.post('friendships/destroy', { screen_name: from }, function(err, data, response) { // Unfollow the user
 						if (err) { // If error results
 							console.log(err); // Print error to the console
-							tweetIt('@' + from + ' Something\'s wrong with Mom\'s computer. Ask @otherconsolelog to help me unfollow you, please.'); // Tweet a request for user to contact @otherconsolelog
+							tweetIt('@' + from + ' Something\'s wrong with ' + bot_name + '\'s computer. Ask @' + bot_owner_name + ' to help me unfollow you, please.'); // Tweet a request for user to contact Bot Owner
 						}
 					});
 				}
 			}
 			for (var l = 0; l < tweet_array.length; l++) { // For each word in the tweet
 				if ('stop'.indexOf(tweet_array[i]) != -1) { // If 'stop' is in the tweet
-					tweetIt('@' + from + ' Mom Bot seems to be upsetting you. Please ask @otherconsolelog for help.'); // Tweet a request for user to contact @otherconsolelog
+					tweetIt('@' + from + ' ' + bot_name + ' seems to be upsetting you. Please ask @' + bot_owner_name + ' for help.'); // Tweet a request for user to contact Bot Owner
 				} else if (sad_words_list.indexOf(tweet_array[i]) != -1) { // If a sad word is in the tweet
 					var feel_better_text = randomSaying(feel_better_bot_list);
 					tweetIt('@' + from + ' ' + feel_better_text); // Tweet to cheer the user up
@@ -157,7 +160,7 @@ function tweetEvent(eventMsg) { // Function to run on each tweet in the stream
 function followed(eventMsg) { // Function to run on follow event
 	var name = eventMsg.source.name; // Who followed
 	var follower_screen_name = eventMsg.source.screen_name; // Follower's screen name
-	if (follower_screen_name !== 'the_mother_bot') { // If follower is not Mother Bot
+	if (follower_screen_name !== 'bot_screen_name') { // If follower is not Bot
 		var thank_you = randomSaying(thank_you_list)
 		tweetIt('@' + follower_screen_name + ' ' + thank_you); // Tweet a thank you expression
 		T.post('friendships/create', { screen_name: follower_screen_name }, function(err, data, response) { // Follow the user back
